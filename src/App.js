f25159352546278ce70bf7701830ed4a7859af14
AybYrn <aybukeyaren--@outlook.com>
2022-08-11 08:21:35 +0300

    Form elements are progressing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  const [formData, setFormData] = React.useState({firstName : "", lastName : "", email : ""})
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    info: "",
+    smoking: true,
+    employment: "",
+  });
 
-  function handleChange(event){
-    const target = event.target
-    const value = event.target.value
-
-    setFormData(prevData => {
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    console.log(name, value, type, checked);
+    setFormData((prevData) => {
       return {
         ...prevData,
-        [target.name] : value
-      }
-    })
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
   }
+  console.log(formData);
   return (
     <div className="form--container">
       <h1>Submission Form</h1>
@@ -22,25 +29,72 @@ function App() {
         <input
           type="text"
           name="firstName"
-          placeholder=" Enter First Name"
+          placeholder="First Name"
           onChange={handleChange}
           value={formData.firstName}
-        ></input>
+        />
         <input
           type="text"
           name="lastName"
-          placeholder=" Enter Last Name"
+          placeholder="Last Name"
           onChange={handleChange}
           value={formData.lastName}
-        ></input>
+        />
         <input
           type="text"
           name="email"
-          placeholder=" Enter Email Address"
+          placeholder="Email Address"
           onChange={handleChange}
           value={formData.email}
-        ></input>
-        <input type="checkbox"></input>
+        />
+        <textarea
+          name="info"
+          placeholder="Give info"
+          onChange={handleChange}
+          value={formData.info}
+        />
+        <div className="smoke">
+          <input
+            type="checkbox"
+            name="smoking"
+            onChange={handleChange}
+            checked={formData.smoking}
+            id="smoke"
+          />
+          <label htmlFor="smoke">Are You Smoking ?</label>
+        </div>
+        <fieldset>
+          <legend>Current Employment Status</legend>
+          <input
+            type="radio"
+            id="unemployed"
+            name="employment"
+            value="unemployed"
+            checked={formData.employment === "unemployed"}
+            onChange={handleChange}
+          />
+          <label htmlFor="unemployed">Unemployed</label>
+          <br />
+          <input
+            type="radio"
+            id="partTime"
+            name="employment"
+            value="part-time"
+            checked={formData.employment === "part-time"}
+            onChange={handleChange}
+          />
+          <label htmlFor="partTime">Part-Time</label>
+          <br />
+          <input
+            type="radio"
+            id="fullTime"
+            name="employment"
+            value="full-time"
+            checked={formData.employment === "full-time"}
+            onChange={handleChange}
+          />
+          <label htmlFor="fullTime">Full-Time</label>
+        </fieldset>
       </form>
     </div>
   );
